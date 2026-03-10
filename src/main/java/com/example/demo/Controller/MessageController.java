d443dea746b9acfddd549c9cdbbff4667b1e0c96
Karol Levi <karollevitafollasalazar@gmail.com>
2026-03-10 12:19:55 -0600

    No se que hacer x2 :,c

--- src/main/java/com/example/demo/Controller/MessageController.java
+++ src/main/java/com/example/demo/Controller/MessageController.java
@@ -3,11 +3,13 @@ package com.example.demo.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Models.Message;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/mensajes")
 public class MessageController {
